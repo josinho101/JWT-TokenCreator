bc885d5dabe6bbba524f67202bce2b5837b3bd25
Joseph J <josinho.seven@gmail.com>
2018-12-20 17:03:10 +0530

    Initial creation

---
+++ JWT-TokenCreator/Home.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace JWT_TokenCreator
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGenerateToken = new System.Windows.Forms.Button();
+            this.txtToken = new System.Windows.Forms.TextBox();
+            this.txtKey = new System.Windows.Forms.TextBox();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnGenerateToken
+            // 
+            this.btnGenerateToken.Location = new System.Drawing.Point(377, 48);
+            this.btnGenerateToken.Name = "btnGenerateToken";
+            this.btnGenerateToken.Size = new System.Drawing.Size(173, 23);
+            this.btnGenerateToken.TabIndex = 0;
+            this.btnGenerateToken.Text = "Generate token";
+            this.btnGenerateToken.UseVisualStyleBackColor = true;
+            this.btnGenerateToken.Click += new System.EventHandler(this.btnGenerateToken_Click);
+            // 
+            // txtToken
+            // 
+            this.txtToken.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtToken.Location = new System.Drawing.Point(0, 87);
+            this.txtToken.Multiline = true;
+            this.txtToken.Name = "txtToken";
+            this.txtToken.Size = new System.Drawing.Size(588, 234);
+            this.txtToken.TabIndex = 1;
+            // 
+            // txtKey
+            // 
+            this.txtKey.Location = new System.Drawing.Point(77, 12);
+            this.txtKey.Name = "txtKey";
+            this.txtKey.Size = new System.Drawing.Size(259, 20);
+            this.txtKey.TabIndex = 2;
+            // 
+            // txtValue
+            // 
+            this.txtValue.Location = new System.Drawing.Point(77, 48);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(259, 20);
+            this.txtValue.TabIndex = 3;
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(475, 11);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Key";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Value";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(377, 10);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(588, 321);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.txtKey);
+            this.Controls.Add(this.txtToken);
+            this.Controls.Add(this.btnGenerateToken);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "JWT Token Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerateToken;
+        private System.Windows.Forms.TextBox txtToken;
+        private System.Windows.Forms.TextBox txtKey;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
+
---
+++ JWT-TokenCreator/Home.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JWT_TokenCreator
+{
+    public partial class Home : Form
+    {
+        private IDictionary<string, string> tokenItems;
+
+        public Home()
+        {
+            tokenItems = new Dictionary<string, string>();
+            InitializeComponent();
+        }
+
+        private void btnGenerateToken_Click(object sender, EventArgs e)
+        {
+            txtToken.Text = GenerateToken(tokenItems);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtKey.Text != "" && txtValue.Text != "")
+            {
+                tokenItems.Add(txtKey.Text, txtValue.Text);
+                txtToken.Text = $"{txtToken.Text}{txtKey.Text} - {txtValue.Text}{Environment.NewLine}";
+                txtKey.Text = string.Empty;
+                txtValue.Text = string.Empty;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtKey.Text = string.Empty;
+            txtValue.Text = string.Empty;
+            txtToken.Text = string.Empty;
+            tokenItems.Clear();
+        }
+
+        private string GenerateToken(IDictionary<string, string> keyValues)
+        {
+            //Set issued at date
+            DateTime issuedAt = DateTime.UtcNow;
+            //set the time when it expires
+            DateTime expires = DateTime.UtcNow.AddDays(7);
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            //create a identity and add claims to the user which we want to log in
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity();
+
+            foreach (var item in keyValues)
+            {
+                claimsIdentity.AddClaim(new Claim(item.Key, item.Value));
+            }
+
+            const string secretKey = "401b09eab3c013d4ca54922bb802bec8fd5318192b0a75f201d8b3727429090fb337591abd3e44453b954555b7a0812e1081c39b740293f765eae731f5a65ed1";
+            var securityKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.Default.GetBytes(secretKey));
+            var signingCredentials = new Microsoft.IdentityModel.Tokens.SigningCredentials(securityKey, Microsoft.IdentityModel.Tokens.SecurityAlgorithms.HmacSha256Signature);
+
+
+            //create the jwt
+            var token = tokenHandler.CreateJwtSecurityToken(issuer: "http://localhost:8080",
+                                                            audience: "http://localhost:8080",
+                                                            subject: claimsIdentity,
+                                                            notBefore: issuedAt,
+                                                            expires: expires,
+                                                            signingCredentials: signingCredentials);
+
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return tokenString;
+        }
+    }
+}
